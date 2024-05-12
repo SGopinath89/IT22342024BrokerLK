8f7695ce612b6795dd966f8e3e6f7568634cb0b0
mbandara1 <139664451+mbandara1@users.noreply.github.com>
2024-05-12 13:36:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import { connectDB } from "./config/db.js";
 
 const app=express();
 const port=8081;
 
 app.use(express.json());
 app.use(cors());
+
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("API working");
@@ -14,3 +17,5 @@ app.get('/', (req, res) => {
 app.listen(port,()=>{
     console.log("server started");
 })
+
+//mongodb+srv://<username>:<password>@cluster0.cekkhwq.mongodb.net/?
