687203ee0e4b2f233b4d513fbe9dcc367b3befd1
mbandara1 <139664451+mbandara1@users.noreply.github.com>
2024-05-12 12:05:46 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app=express();
+const port=8081;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send("API working");
+})
+
+app.listen(port,()=>{
+    console.log("server started");
+})
