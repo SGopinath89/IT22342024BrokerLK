dd681ab59b840e5885e4f81b421669d11f974509
mbandara1 <139664451+mbandara1@users.noreply.github.com>
2024-05-18 18:47:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import vehicleRouter from "../routs/vehicleRouts.js";
 
 const app=express();
 const port=8081;
@@ -9,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 connectDB();
+
+//api endpoint
+app.use("/api/vehicle",vehicleRouter)
 
 app.get('/', (req, res) => {
     res.send("API working");
