e1c321c0a2788bf63cb3163ebfaf49257bc56358
mbandara1 <139664451+mbandara1@users.noreply.github.com>
2024-05-18 16:01:46 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async ()=>{
-    await mongoose.connect('mongodb+srv://Brokerlk1:B123@cluster0.teezko8.mongodb.net/brokerlk').then(()=>console.log("DB connected successfully"));
+    await mongoose.connect('mongodb+srv://Admin:Admin123@cluster0.cekkhwq.mongodb.net/IT22342024BrokerLK').then(()=>console.log("DB connected successfully"));
 }
-//mongodb+srv://<username>:<password>@cluster0.teezko8.mongodb.net/?
